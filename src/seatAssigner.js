1dcee8814a923e84debe34335d83dc8b4a9f5aec
= <=>
2022-02-13 15:05:02 -0800

    remove unnecessary var

--- src/seatAssigner.js
+++ src/seatAssigner.js
@@ -5,14 +5,16 @@ const COLUMNS = ["A", "B", "C", "D", "E", "F", "G"];
 function seatAssigner(inputStr) {
   const groupId = Object.keys(seats).length;
   seats[groupId] = [];
+  
   let i = Object.values(seats).flat().length;
-  const initLength = Object.values(seats).flat().length;
   const target = parseInt(inputStr)+Object.values(seats).flat().length;
+
   for(i; i<target; i++) {
     let row = Math.ceil((Object.values(seats).flat().length + 1) / COLUMNS.length);
     let column = COLUMNS[Object.values(seats).flat().length % COLUMNS.length];
     seats[groupId].push(`${row}${column}`);
   }
+
   // console.dir(seats);
   return seats[groupId];
 }
